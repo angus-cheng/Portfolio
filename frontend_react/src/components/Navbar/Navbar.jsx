e0da1a8006f6ba23eaed1f1e8079c900bfb31193
Angus Lind Cheng <anguslindcheng@Angus-Macbook-Pro.local>
2023-01-06 14:40:51 +1000

    Removed unneccesary image call

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 import './Navbar.scss';
-import { images } from '../../constants';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
