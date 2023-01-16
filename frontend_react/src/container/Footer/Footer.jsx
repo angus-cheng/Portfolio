20809a6a02949f3e3629b8cea5cacdbcc6a4a610
Angus Cheng <anguscheng389@gmail.com>
2023-01-16 12:28:48 +1000

    Removed whitespace

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
-<<<<<<< Updated upstream
 import './Footer.scss';
 
 const Footer = () => {
@@ -36,22 +35,11 @@ const Footer = () => {
       .catch((err) => console.log(err));
   };
 
-=======
-import './Footer.scss'
-
-const Footer = () => {
-  const [formData, setFormData] = useState({name: "", email: "", message: ""});
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-
->>>>>>> Stashed changes
   return (
     <>
       <h2 className="head-text">Take a coffee & chat with me</h2>
 
       <div className="app__footer-cards">
-<<<<<<< Updated upstream
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
           <a href="mailto:anguslindcheng@gmail.com" className="p-text">anguslindcheng@gmail.com</a>
@@ -100,44 +88,3 @@ export default AppWrap(
   'contact',
   'app__whitebg',
 );
-=======
-        <div classname="app__footer-card">
-          <img src={images.email} alt="email" />
-          <a href="mailto:anguslindcheng@gmail.com" className="p-text">anguslindcheng@gmail.com</a>
-        </div>
-        <div classname="app__footer-card">
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel:0403 779 594" className="p-text">0403 779 594</a>
-        </div>
-      </div>
-
-      <div className="app__footer-form app__Flex">
-        <div className="app__flex">
-          <input className="p-text" type="text" placeholder="Your name" name="name" value={name} onChange={handleChangeInput}/>
-        </div>
-        <div className="app__flex">
-          <input className="p-text" type="text" placeholder="Your email" email="email" value={email} onChange={handleChangeInput}/>
-        </div>
-        <div>
-          <textarea
-            className="p-text"
-            placeholder="Your Message"
-            value={message}
-            name={message}
-            onChange={handleChangeInput}
-          />
-        </div>
-        <button type="button" className="p-text" onCLick="handleSubmit">{loading ? "Sending" : "Submit Message"}</button>
-      </div>
-
-
-    </>
-  )
-}
-
-export default AppWrap (
-  MotionWrap(Footer, 'app__footer'),
-  'contact',
-  'app__whitebg'
-)
->>>>>>> Stashed changes
